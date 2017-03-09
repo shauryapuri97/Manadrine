35859924e4906e3d081627bbf99ebc9b188916aa
Wisemonkey007 <alex_cordaro@hotmail.co.uk>
2017-03-09 16:04:03 +0000

    Update Player.py

--- gamelib/Player.py
+++ gamelib/Player.py
@@ -4,14 +4,36 @@ class Player:
         self.spritesheet = simplegui.load_image('http://i.imgur.com/esDivR3.png')
         self.counter = 0
         self.time = 0
-        self.x = 100
-        self.y = 100
+        self.pos = Vector((100, 100))
 
         
     def draw(self, canvas):
         
         imgCentre = (((2*self.counter + 1)%8)*272/8, (5)*288/8)
-        canvas.draw_image(self.spritesheet, imgCentre, (272/4, 288/4), (self.x, self.y), (125, 125))
+        canvas.draw_image(self.spritesheet, imgCentre, (272/4, 288/4), self.pos.getP(), (125, 125))
         self.time += 1
         if self.time % 5 == 0:
             self.counter += 1
+        
+    def keydown(self, key):
+        
+        v = Vector((0, 20))
+        u = Vector((20, 0))
+        
+        if key == simplegui.KEY_MAP['down']:
+            self.pos.add(v)
+        
+        if key == simplegui.KEY_MAP['up']:
+            self.pos.sub(v)
+        
+        if key == simplegui.KEY_MAP['left']:
+            self.pos.sub(u)
+        
+        if key == simplegui.KEY_MAP['right']:
+            self.pos.add(u)
+            background.bgSpeed = 4
+            
+    def keyup(self, key):
+        
+        if key == simplegui.KEY_MAP['right']:
+            background.bgSpeed = 2
